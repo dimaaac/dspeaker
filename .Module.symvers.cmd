69851c6cb067aa545a6046670b2a9922dd32bb67
dimaaac <78304878+dimaaac@users.noreply.github.com>
2024-01-30 16:27:27 -0500

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/dima/Code/dspeaker/Module.symvers :=  sed 's/ko$$/o/'  /home/dima/Code/dspeaker/modules.order | scripts/mod/modpost -m     -o /home/dima/Code/dspeaker/Module.symvers -e -i Module.symvers -T - 
